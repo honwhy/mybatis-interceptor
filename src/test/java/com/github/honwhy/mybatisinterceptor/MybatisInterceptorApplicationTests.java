a97163a37d6c41ee651b6e83b520818510625ea1
Honwhy Wang <honwhy.wang@gmail.com>
2019-07-23 22:32:16 +0800

    测试恶意注入sql

--- src/test/java/com/github/honwhy/mybatisinterceptor/MybatisInterceptorApplicationTests.java
+++ src/test/java/com/github/honwhy/mybatisinterceptor/MybatisInterceptorApplicationTests.java
@@ -2,6 +2,7 @@ package com.github.honwhy.mybatisinterceptor;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +10,26 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class MybatisInterceptorApplicationTests {
 
+    @Autowired
+    private CityMapper cityMapper;
     @Test
     public void contextLoads() {
     }
 
+    @Test
+    public void test_malicious() {
+        City city = new City();
+        city.setCountry("china(name)values(\'bad\');drop table city_china;insert into city");
+        city.setName("xxx");
+        city.setState("yyy");
+        cityMapper.insertCityDynamic(city.getCountry(), city);
+    }
+    @Test
+    public void test_ok() {
+        City city = new City();
+        city.setCountry("china");
+        city.setName("xxx");
+        city.setState("yyy");
+        cityMapper.insertCityDynamic(city.getCountry(), city);
+    }
 }
